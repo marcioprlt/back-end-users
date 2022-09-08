55d052621e615abc4950cc7e132c76f966809c26
Marcio Peralta <marcioprlt@gmail.com>
2022-09-08 17:59:39 -0300

    Adicionando validação de idade e paginação

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,13 +1,24 @@
 import users from "../models/User.js";
+import moment from "moment";
 
 class UserController {
 
     static listUsers = (req, res) => {
-        users.find().lean().exec((err, users) => {
 
-            users.forEach(user => delete user.password);
-            res.status(200).json(users);
-        });
+        const page = parseInt(req.query.page);
+        const preLimit = parseInt(req.query.limit);
+        const limit = preLimit ? preLimit : 3;
+
+        const startIndex = (page - 1) * limit;
+
+        try {
+            users.find().limit(limit).skip(startIndex).lean().exec((err, users) => {
+                users.forEach(user => delete user.password);
+                res.status(200).json(users);
+            });
+        } catch (e) {
+            res.status(500).send({message: `Pagination error -- ${e.message}`});
+        }
     }
 
     static listUserById = (req, res) => {
@@ -19,22 +30,30 @@ class UserController {
                     delete user.password;
                     res.status(200).send(user);
                 } else {
-                    res.status(404).send({message: "Usuário não encontrado"});
+                    res.status(404).send({message: "User not found"});
                 }
             } else {
-                res.status(404).send({message: `Usuário não encontrado -- ${err.message}`});
+                res.status(404).send({message: `User not found -- ${err.message}`});
             }
         }); 
     }
 
     static createUser = (req, res) => {
-        let user = users(req.body);
+        let user = new users(req.body);
+
+        if (!this.validateYears(user.birthDate))
+        {
+            res.status(400).send({message: "User is under 18"});
+            return;
+        }
 
         user.save(err => {
             if (!err) {
-                res.status(201).send(user.toJSON());
+                user = user.toJSON();
+                user.password = user.password.replace(/./g, "*");
+                res.status(201).send(user);
             } else {
-                res.status(500).send({message: `Falha ao cadastrar o usuário -- ${err.message}`});
+                res.status(500).send({message: `Could not create user -- ${err.message}`});
             }
         });
     }
@@ -42,13 +61,21 @@ class UserController {
     static updateUser = (req, res) => {
         const id = req.params.id;
 
-        users.findByIdAndUpdate(id, {$set: req.body}, (err) => {
-            if (!err) {
-                res.status(200).send({message: "Usuário atualizado com sucesso"});
-            } else {
-                res.status(404).send({message: `Usuário não encontrado -- ${err.message}`});
+        if (req.body.birthDate) {
+            if (!this.validateYears(req.body.birthDate))
+            {
+                res.status(400).send({message: "User is under 18"});
+                return;
             }
-        });
+        }
+
+        users.findByIdAndUpdate(id, {$set: req.body}, {runValidators: true}, (err) => {
+            if (!err) {
+                res.status(200).send({message: "User updated successfully"});
+            } else {
+                res.status(404).send({message: `Could not update user -- ${err.message}`});
+            }
+        })
     }
 
     static deleteUser = (req, res) => {
@@ -56,9 +83,9 @@ class UserController {
 
         users.findByIdAndDelete(id, (err) => {
             if (!err) {
-                res.status(204).send({message: "Usuário removido com sucesso"});
+                res.status(204).send({message: "User deleted successfully"});
             } else {
-                res.status(404).send({message: `Usuário não encontrado -- ${err.message}`});
+                res.status(404).send({message: `User not found -- ${err.message}`});
             }
         });
     }
@@ -69,12 +96,18 @@ class UserController {
         users.find((err, users) => {
             let usersWithName = [];
             users.forEach(user => {
-                if (user.name.toLowerCase().includes(name.toLowerCase())) {
+                if (name != "" && user.name.toLowerCase().includes(name.toLowerCase())) {
                     usersWithName.push(user);
                 }
             });
             res.status(200).json(usersWithName);
         });
+    }
+
+    static validateYears(date) {
+        const dur = moment.duration({from: moment(date), to: new Date()});
+        const years = dur.asYears();
+        return years >= 18;
     }
 }
 
--- src/models/User.js
+++ src/models/User.js
@@ -3,17 +3,17 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     id: {type: String},
     name: {type: String, required: true},
-    cpf: {type: String, minlength: 11, maxlength: 11, required: true},
+    cpf: {type: String, validate:/^\d{11}$/, required: true},
     birthDate: {type: Date, required: true},
     email: {type: String, validate:/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/, required: true},
-    password: {type: String, minlength: 6, required: true},
+    password: {type: String, validate:/\S{6,}/, required: true},
     address: {type: String, required: true},
     number: {type: String, required: true},
     complement: {type: String, required: true},
     city: {type: String, required: true},
     state: {type: String, required: true},
     country: {type: String, required: true},
-    zipCode: {type: String, required: true}
+    zipCode: {type: String, validate:/^\d{5}-\d{3}$/, required: true}
 });
 
 const users = mongoose.model("users", userSchema);
